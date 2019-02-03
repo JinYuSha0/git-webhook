020b82ed93c85dd37af25d2fa5f8427a8aff69a7
邵瑾瑜 <1009943858@qq.com>
2019-02-03 13:11:13 +0800

    完善基本功能

--- node.d.ts
+++ node.d.ts
@@ -1,2 +1,3 @@
 import * as http from 'http'
 import * as events from 'events'
+import * as crypto from 'crypto'
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import * as http from 'http'
 import * as EventEmitter from 'events'
+import * as crypto from 'crypto'
+
+interface IConfig {
+  readonly path: string,
+  readonly secret: string,
+  readonly callback?: Function,
+}
+
+interface IEmitData {
+  event: string,
+  id: string,
+  payload: Object,
+  headers: http.IncomingHttpHeaders,
+}
 
 class GitWebHook extends EventEmitter {
-  private server: http.Server
-  constructor(server?: http.Server, port: number = 80, listeningListener?: Function) {
+  private readonly server: http.Server
+  private readonly config: IConfig
+  constructor(config: IConfig, server?: http.Server, port: number = 8001, listeningListener?: Function) {
     super()
+    this.config = config
     if (server) {
       this.server = server
     } else {
@@ -12,13 +28,85 @@ class GitWebHook extends EventEmitter {
       this.server.listen(port, listeningListener || function () {
         console.log(`Server listening port ${port}`)
       })
+      this.server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
+        function timeout() {
+          res.writeHead(400)
+          res.end('Can\'t not handle this')
+        }
+        setTimeout(timeout, 3000)
+      })
     }
-    this.server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
-      console.log(req.method, req.headers)
-      res.writeHead(200)
-      res.end(`Hello GitWebHook`)
+    this.handler(this.server, config)
+  }
+  public handler(server: http.Server, config: IConfig): void {
+    server.on('request', async function (req: http.IncomingMessage, res: http.ServerResponse) {
+      const hasCb = !!config.callback, cb = hasCb ? config.callback(req, res) : undefined
+      if (req.url.split('?').shift() !== config.path || req.method !== 'POST') {
+        return hasCb && cb(new Error('No such location'))
+      }
+      let hasError = function (msg: string): void {
+        res.writeHead(400, {'content-type': 'application/json'})
+        res.end(JSON.stringify({error: msg}))
+        const err = new Error(msg)
+        this.emit('error', err, req)
+        return hasCb && cb(err)
+      }
+      hasError = hasError.bind(this)
+      const sig = req.headers['x-hub-signature']
+        , event = req.headers['x-github-event']
+        , id    = req.headers['x-github-delivery']
+      if (!sig) {
+        return hasError('No X-Hub-Signature found on request')
+      }
+      if (!event) {
+        return hasError('No X-Hub-Event found on request')
+      }
+      if (!id) {
+        return hasError('No X-Github-Delivery found on request')
+      }
+      const body = await this.parseBody(req)
+      if (!this.verify(<string>sig, body)) {
+        return hasError('X-Hub-Signature verify failure')
+      }
+      let obj: object
+      try {
+        obj = JSON.parse(body)
+      } catch (e) {
+        hasError(e)
+      }
+      const emitData = {
+          event
+        , id
+        , payload: obj
+        , headers: req.headers
+      }
+      this.emit(<string>event, emitData)
+      this.emit('*', emitData)
+    }.bind(this))
+  }
+  public sign (data: string) {
+    return 'sha1=' + crypto.createHmac('sha1', this.config.secret).update(data).digest('hex')
+  }
+  public verify (signature: string, data: string) {
+    return Buffer.from(signature).equals(Buffer.from(this.sign(data)))
+  }
+  public parseBody (req: http.IncomingMessage) {
+    let body = ''
+    return new Promise<string>((resolve, reject) => {
+      req.on('data', function (chunk) {
+        body += chunk
+      })
+      req.on('end', function () {
+        resolve(body)
+      })
     })
   }
 }
 
-const gitWebHook: GitWebHook = new GitWebHook()
+const gitWebHook: GitWebHook = new GitWebHook({ path: '/', secret: 'sjy96816' })
+gitWebHook.on('push', (emitData: IEmitData) => {
+  console.log('push', emitData.payload)
+})
+gitWebHook.on('error', (err: Error, req: http.IncomingMessage) => {
+  console.log('error', err)
+})
